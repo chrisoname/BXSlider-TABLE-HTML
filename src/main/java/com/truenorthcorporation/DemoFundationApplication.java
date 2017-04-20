9d01dc6c82cefeb6d02d763d2fedcbac3eb350d3
LAPTOP-1K08EJLL\Abdyel <abgonzalez@truenorthcorporation.com>
2017-04-20 09:43:01 -0400

    Ejemplo eliminar luego

--- src/main/java/com/truenorthcorporation/DemoFundationApplication.java
+++ src/main/java/com/truenorthcorporation/DemoFundationApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -25,10 +26,18 @@ class GreetingController {
     
     @RequestMapping("/hello/{name}")
     String hello1(@PathVariable String name) {
-        return "";
+        return name;
     }
 
-    
+    @RequestMapping("/test")
+    public GreetingData testGreeting(@RequestParam(value = "name",required=true) String name, 
+    		@RequestParam(value = "greeting",required=true) String greeting) {
+    	GreetingData greet = new GreetingData();
+    	greet.setName(name);
+    	greet.setGreeting(greeting);
+        return greet;
+    }
+
 	
 }
 
