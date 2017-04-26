bc8d693cc9af6f15726f4f01125ba3af1f4d0d31
lumartinez <lumartinez@truenorthcorporation.com>
2017-04-26 08:26:23 -0400

    Añadir consultas

--- src/main/java/com/truenorth/foundation/web/api/client/NeoClient.java
+++ src/main/java/com/truenorth/foundation/web/api/client/NeoClient.java
@@ -18,6 +18,20 @@ public class NeoClient {
 		RestTemplate restTemplate = new RestTemplate();
 		List<DestinationData> response =  Arrays.asList(restTemplate.getForObject(Constants.NEO_SERVICE + "/findCuratorDestinations", DestinationData[].class));
 		return response;
-	}	
+	}
+	
+	public static DestinationData findDestinationByFprId(String fprid)
+    {
+        RestTemplate restTemplate = new RestTemplate();
+        DestinationData response = restTemplate.getForObject(Constants.NEO_SERVICE + "/findDestinationByFprId?fprid=" + fprid, DestinationData.class);
+        return response;
+    }
+	
+	public static List<DestinationData> findDestinationsByStatus(String status)
+    {
+        RestTemplate restTemplate = new RestTemplate();
+        List<DestinationData> response = Arrays.asList(restTemplate.getForObject(Constants.NEO_SERVICE + "/findDestinationsByStatus?status_detail=" + status, DestinationData[].class));
+        return response;
+    }
 
 }
